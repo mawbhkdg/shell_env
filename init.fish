5e98d5b46a28c76790a79019a5b567bfa90b363e
mawbhkdg <mawbhkdg@users.noreply.github.com>
2019-03-04 15:41:53 +0800

    Update init.fish

--- init.fish
+++ init.fish
@@ -6,9 +6,9 @@ set -xg CLASSPATH . $JAVA_HOME/jre/lib/rt.jar $JAVA_HOME/lib/dt.jar $JAVA_HOME/l
 set -xg HADOOP_HOME /usr/local/Cellar/hadoop/2.8.0
 set -xg HIVE_HOME /usr/local/Cellar/hive/2.1.0/libexec
 
-set -xg PATH /usr/bin $PATH /usr/local/bin $JAVA_HOME/bin /Users/wenbin9/anaconda/bin /Users/wenbin9/bin
+set -xg PATH /usr/bin $PATH /usr/local/bin $JAVA_HOME/bin /Users/myuser/anaconda/bin /Users/myuser/bin
 
 alias hadoop_start="/usr/local/Cellar/hadoop/2.8.0/sbin/start-dfs.sh;/usr/local/Cellar/hadoop/2.8.0/sbin/start-yarn.sh"
 alias hadoop_stop="/usr/local/Cellar/hadoop/2.8.0/sbin/stop-yarn.sh;/usr/local/Cellar/hadoop/2.8.0/sbin/stop-dfs.sh"
 alias es="/usr/local/bin/emacs -nw "
-alias python="/Users/wenbin9/anaconda/bin/python "
+alias python="/Users/myuser/anaconda/bin/python "
